755467f8666d3881939af9c3bdde57c7489e4636
muchai <1822868+muchai@users.noreply.github.com>
2020-03-17 15:24:58 +0300

    g
    g

---
+++ erpnext/hr/report/tax_deduction_card/tax_deduction_card.js
@@ -0,0 +1,68 @@
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Tax Deduction Card"] = {
+	"filters": [
+		{
+			"fieldname": "fiscal_year",
+			"label": __("Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.defaults.get_user_default("fiscal_year"),
+			"reqd": 1,
+			"on_change": function(query_report) {
+				var fiscal_year = query_report.get_values().fiscal_year;
+				if (!fiscal_year) {
+					return;
+				}
+				frappe.model.with_doc("Fiscal Year", fiscal_year, function(r) {
+					var fy = frappe.model.get_doc("Fiscal Year", fiscal_year);
+					frappe.query_report.set_filter_value({
+						from_date: fy.year_start_date,
+						to_date: fy.year_end_date
+					});
+				});
+			}
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"reqd": 1,
+			"read_only": 1,
+			"hidden": 0
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date"),
+			"reqd": 1,
+			"read_only": 1,
+			"hidden": 0
+		},
+		{
+			"fieldname":"employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee"
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		},
+		{
+			"fieldname":"docstatus",
+			"label":__("Document Status"),
+			"fieldtype":"Select",
+			"options":["Draft", "Submitted", "Cancelled"],
+			"default":"Submitted",
+			"read_only": 1,
+			"hidden": 0
+		}
+	]
+}
---
+++ erpnext/hr/report/tax_deduction_card/tax_deduction_card.py
@@ -0,0 +1,117 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import flt
+from frappe import _
+#Navari Changes
+from frappe.utils import getdate, cint, formatdate
+import calendar
+
+def execute(filters=None):
+	if not filters: filters = {}
+	salary_slips = get_salary_slips(filters)
+	if not salary_slips: return [], []
+
+	#Navari
+	employees = get_employees(salary_slips)
+	months = get_months(salary_slips)
+	#End Navari
+	columns, earning_types, ded_types = get_columns(salary_slips)
+	ss_earning_map = get_ss_earning_map(salary_slips)
+	ss_ded_map = get_ss_ded_map(salary_slips)
+	
+	data = []
+	for ss in salary_slips:
+		row = [ss.start_date, ss.employee_name]
+
+		#Convert start Date to Month
+		#month = [formatdate(ss.start_date, "MMM YY")
+
+		for e in earning_types:
+			row.append(ss_earning_map.get(ss.name, {}).get(e))
+
+		row += [ss.gross_pay]
+
+		for d in ded_types:
+			row.append(ss_ded_map.get(ss.name, {}).get(d))
+
+		row.append(ss.total_loan_repayment)
+
+		row += [ss.total_deduction, ss.net_pay]
+
+		data.append(row)
+
+	return columns, data
+
+def get_columns(salary_slips):
+	"""
+	columns = [
+		_("Start Date") + "::80", _("Employee Name") + "::140"
+	]
+	"""
+	columns = [
+		_("Start Date") + "::80", _("Employee Name") + "::140"
+	]
+
+	salary_components = {_("Earning"): [], _("Deduction"): []}
+
+	for component in frappe.db.sql("""select distinct sd.salary_component, sc.type
+		from `tabSalary Detail` sd, `tabSalary Component` sc
+		where sc.name=sd.salary_component and sd.amount != 0 and sd.parent in (%s)""" %
+		(', '.join(['%s']*len(salary_slips))), tuple([d.name for d in salary_slips]), as_dict=1):
+		salary_components[_(component.type)].append(component.salary_component)
+
+	columns = columns + [(e + ":Currency:120") for e in salary_components[_("Earning")]] + \
+		[_("Gross Pay") + ":Currency:120"] + [(d + ":Currency:120") for d in salary_components[_("Deduction")]] + \
+		[_("Loan Repayment") + ":Currency:120", _("Total Deduction") + ":Currency:120", _("Net Pay") + ":Currency:120"]
+
+	return columns, salary_components[_("Earning")], salary_components[_("Deduction")]
+
+def get_salary_slips(filters):
+	filters.update({"from_date": filters.get("from_date"), "to_date":filters.get("to_date")})
+	conditions, filters = get_conditions(filters)
+	salary_slips = frappe.db.sql("""select * from `tabSalary Slip` where %s
+		order by employee""" % conditions, filters, as_dict=1)
+
+	return salary_slips or []
+
+def get_conditions(filters):
+	conditions = ""
+	doc_status = {"Draft": 0, "Submitted": 1, "Cancelled": 2}
+
+	if filters.get("docstatus"):
+		conditions += "docstatus = {0}".format(doc_status[filters.get("docstatus")])
+
+	if filters.get("from_date"): conditions += " and start_date >= %(from_date)s"
+	if filters.get("to_date"): conditions += " and end_date <= %(to_date)s"
+	if filters.get("company"): conditions += " and company = %(company)s"
+	if filters.get("employee"): conditions += " and employee = %(employee)s"
+
+	return conditions, filters
+
+
+def get_ss_earning_map(salary_slips):
+	ss_earnings = frappe.db.sql("""select parent, salary_component, amount
+		from `tabSalary Detail` where parent in (%s)""" %
+		(', '.join(['%s']*len(salary_slips))), tuple([d.name for d in salary_slips]), as_dict=1)
+
+	ss_earning_map = {}
+	for d in ss_earnings:
+		ss_earning_map.setdefault(d.parent, frappe._dict()).setdefault(d.salary_component, [])
+		ss_earning_map[d.parent][d.salary_component] = flt(d.amount)
+
+	return ss_earning_map
+
+def get_ss_ded_map(salary_slips):
+	ss_deductions = frappe.db.sql("""select parent, salary_component, amount
+		from `tabSalary Detail` where parent in (%s)""" %
+		(', '.join(['%s']*len(salary_slips))), tuple([d.name for d in salary_slips]), as_dict=1)
+
+	ss_ded_map = {}
+	for d in ss_deductions:
+		ss_ded_map.setdefault(d.parent, frappe._dict()).setdefault(d.salary_component, [])
+		ss_ded_map[d.parent][d.salary_component] = flt(d.amount)
+
+	return ss_ded_map
